bea71c6b45671d03caef435bc2fde4f6a7fb0e00
hyunjung <hazelc3837@gmail.com>
2022-04-15 20:47:39 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Hyunjung/Login/HyunjungLogin.js
+++ src/pages/Hyunjung/Login/HyunjungLogin.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Login.scss";
+import { useNavigate } from "react-router-dom";
 
-const HyunJungLogin = props => {
-  return <header>headers</header>;
+const HyunJungLogin = () => {
+  const navigate = useNavigate();
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
+  
+
+  const handlerIdInput = (event) => {
+    setUserId(event.target.value);
+  };
+  const handlerPwInput = (event) => {
+    setUserPw(event.target.value);
+  };
+
+
+  const goToMain = () => {
+    navigate("/main");
+  };
+  return (
+    <main className="login-main">
+      <article className="login-article">
+        <div className="logo">
+          <h1 className="login-logo">westagram</h1>
+        </div>
+        <form className="login-form">
+          <div className="form-id">
+            <input
+              onChange={handlerIdInput}
+              className="input-id"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onKeyUp={isPassLogin}
+            />
+          </div>
+          <div className="form-pw">
+            <input
+              onChange={handlerPwInput}
+              className="input-pw"
+              type="password"
+              placeholder="비밀번호"
+              onKeyUp={isPassLogin}
+            />
+          </div>
+          <div className="form-button">
+            <button className="form-btn" onClick={goToMain}>
+              로그인
+            </button>
+          </div>
+          <div className="form-pw-forget">
+            <a href="#" className="pw-forget">
+              비밀번호를 잊으셨나요?
+            </a>
+          </div>
+        </form>
+      </article>
+    </main>
+  );
 };
 
 export default HyunJungLogin;
--- src/pages/Hyunjung/Main/HyunjungMain.js
+++ src/pages/Hyunjung/Main/HyunjungMain.js
@@ -1,7 +1,218 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Main.scss";
+import Nav from "../../components/Nav/Nav";
 
-const HyunJungMain = props => {
-  return <header>headers</header>;
+const HyunJungMain = (props) => {
+ 
+  return (
+    <>
+      <Nav />
+      <main className="main-feeds">
+        <section className="left">
+          <div className="feeds">
+            <div className="header">
+              <div className="header-id">
+                <img
+                  alt="profile-img"
+                  src="images/main.png"
+                  className="id-img"
+                />
+                <div className="id">happyDay01</div>
+              </div>
+              <div className="header-dot">
+                <i className="fa-solid fa-ellipsis"></i>
+              </div>
+            </div>
+            <div className="feeds-pic">
+              <img alt="main-pic" src="images/mainpage.jpg" />
+            </div>
+            <section className="feeds-icon">
+              <div className="feeds-icon-left">
+                <span className="feeds-icon-heart">
+                  <i className="fa-icon fa-regular fa-heart"></i>
+                </span>
+                <span className="feeds-icon-comment">
+                  <i className="fa-icon fa-regular fa-comment"></i>
+                </span>
+                <span className="feeds-icon-plane">
+                  <i className="fa-icon fa-regular fa-paper-plane"></i>
+                </span>
+              </div>
+              <div className="feeds-icon-right">
+                <span className="feeds-icon-bookmark">
+                  <i className="fa-icon fa-regular fa-bookmark"></i>
+                </span>
+              </div>
+            </section>
+            <div className="feeds-like">좋아요 30,000 개</div>
+            <div>
+              <div className="feeds-article">
+                <span className="feeds-article-id">
+                  <strong>happyDay01</strong>
+                </span>
+                <span className="feeds-article-content"> 행복한 하루... </span>
+                <span className="feeds-article-over">더 보기</span>
+              </div>
+              <div>
+                <ul className="feeds-comment">
+                      <li className="comment">
+                        <div className="comment-left">
+                          <span className="comment-id">smileDay01</span>
+                          <span className="comment-content">행복한 하루!</span>
+                        </div>
+                        <div className="comment-right">
+                          <button className="comment-like">
+                            <i className="fa-icon fa-regular fa-heart"></i>
+                          </button>
+                          <button className="comment-delete">
+                            <i className="fa-solid fa-trash-can"></i>
+                          </button>
+                        </div>
+                      </li> 
+                </ul>
+                <div className="comment-time">55분 전</div>
+                {/* 인풋창 */}
+                <div className="feeds-comment-input">
+                  <div className="comment-input">
+                    <input
+                      className="comment-text"
+                      placeholder="댓글 쓰기..."
+                    />
+                  </div>
+                  <div className="comment-button">
+                    <button className="comment-btn">
+                      게시
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="right">
+          <div className="main-right">
+            <div>
+              <div className="aside-header">
+                <img
+                  alt="aside-profile-pic"
+                  src="images/main.png"
+                  className="header-aside-img"
+                />
+                <div className="aside-id">
+                  <div className="aside-id-top">happyDay01</div>
+                  <div className="aside-id-bottom">happyLife</div>
+                </div>
+              </div>
+              <div className="aside-form">
+                <form className="story">
+                  <div className="story-header">
+                    <div className="story-title">스토리</div>
+                    <div>모두 보기</div>
+                  </div>
+                  <div className="aside-header">
+                    <img
+                      alt="story-user-pic01"
+                      src="images/pro01.png"
+                      className="aside-img"
+                    />
+                    <div className="aside-id">
+                      <div className="aside-id-top">puppy_love</div>
+                      <div className="aside-id-time">12분 전</div>
+                    </div>
+                  </div>
+                  <div className="aside-header">
+                    <img
+                      alt="story-user-pic02"
+                      src="images/pro02.png"
+                      className="aside-img"
+                    />
+                    <div className="aside-id">
+                      <div className="aside-id-top">smile_angry</div>
+                      <div className="aside-id-time">32분 전</div>
+                    </div>
+                  </div>
+                  <div className="aside-header">
+                    <img
+                      alt="story-user-pic03"
+                      src="images/pro03.png"
+                      className="aside-img"
+                    />
+                    <div className="aside-id">
+                      <div className="aside-id-top">kitty_77</div>
+                      <div className="aside-id-time">55분 전</div>
+                    </div>
+                  </div>
+                </form>
+                <form className="recommend">
+                  <div className="recommend-header">
+                    <div className="recommend-title">회원님을 위한 추천</div>
+                    <div>모두보기</div>
+                  </div>
+                  <div className="member">
+                    <div className="member-header">
+                      <img
+                        alt="recommend-user-pic01"
+                        src="images/pro04.png"
+                        className="aside-img"
+                      />
+                      <div className="aside-id">
+                        <div className="aside-id-top">wink22_puppy</div>
+                        <div className="aside-id-bottom">
+                          puppy_love님 외 30명...
+                        </div>
+                      </div>
+                    </div>
+                    <div className="follow">팔로우</div>
+                  </div>
+                  <div className="member">
+                    <div className="member-header">
+                      <img
+                        alt="recommend-user-pic02"
+                        src="images/pro05.png"
+                        className="aside-img"
+                      />
+                      <div className="aside-id">
+                        <div className="aside-id-top">wow_7777</div>
+                        <div className="aside-id-bottom">
+                          puppy_love님 외 30명...
+                        </div>
+                      </div>
+                    </div>
+                    <div className="follow">팔로우</div>
+                  </div>
+                  <div className="member">
+                    <div className="member-header">
+                      <img
+                        alt="recommend-user-pic03"
+                        src="images/pro06.png"
+                        className="aside-img"
+                      />
+                      <div className="aside-id">
+                        <div className="aside-id-top">the_best_one01</div>
+                        <div className="aside-id-bottom">
+                          puppy_love님 외 30명...
+                        </div>
+                      </div>
+                    </div>
+                    <div className="follow">팔로우</div>
+                  </div>
+                </form>
+                <div>
+                  <div className="aside-info">
+                    <div>westargram 정보 ∙ 지원 ∙ 홍보센터 ∙ API ∙</div>
+                    <div>채용정보 ∙ 개인정보처리방침 ∙ 약관 ∙</div>
+                    <div>디렉터리 ∙ 프로필 ∙ 해시태그 ∙ 언어</div>
+                  </div>
+                </div>
+                <div className="aside-right">© 2022 westagram</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default HyunJungMain;
+
